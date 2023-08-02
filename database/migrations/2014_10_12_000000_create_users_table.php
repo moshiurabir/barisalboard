a66590b3654a97a7072d13c81f1443cec832b3e5
moshiurabir <moshiurabir@gmail.com>
2023-08-02 17:13:52 +0600

    user table modify

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,21 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('designation')->nullable();
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('is_admin')->nullable();
+            $table->string('role')->nullable();
+            $table->string('section')->nullable();
+            $table->string('section_id')->nullable();
+            $table->string('section_department')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('sign')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('note')->nullable();
+            $table->string('status')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
