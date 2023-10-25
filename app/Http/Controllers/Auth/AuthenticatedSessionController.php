58399be6538b9031cd95d3c10131e65e3c297e49
moshiurabir <moshiurabir@gmail.com>
2023-10-26 00:08:28 +0600

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Session;
+use App\Http\Requests\Auth\LoginRequest;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -28,10 +29,37 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+
+
+
+
+
+
         $url='';
         if($request->user()->role === 'admin')
         {
-            $url='boardcp/dashboard';
+
+
+
+
+
+
+                if(session_id() != Auth::user()->last_session){
+                  Auth::logout();
+                  return redirect('boardcp/login');
+                }
+            else{
+                $session_id = Session::getId();
+                $request->user()->last_session = $session_id;
+                $request->user()->save();
+                $url='boardcp/dashboard';
+             }
+
+
+
+
+
         }
         elseif($request->user()->role === 'exam')
         {
