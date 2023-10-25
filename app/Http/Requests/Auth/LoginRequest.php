e1ebea4f406159dbac142cf3ff8d64c58e88b661
moshiurabir <moshiurabir@gmail.com>
2023-10-25 22:14:26 +0600

    status add in login

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -41,7 +41,9 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        if (! Auth::attempt($this->only('email', 'password')+ ['status' => 'active'], $this->boolean('remember'))) {
+
+
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
