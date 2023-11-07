595b610a8f8cae67c2b6d208c32fb2d3b738d51c
moshiurabir <moshiurabir@gmail.com>
2023-11-08 01:41:38 +0600

    update

--- resources/views/adminbackend/ssc_registration/ssc_payment.blade.php
+++ resources/views/adminbackend/ssc_registration/ssc_payment.blade.php
@@ -230,7 +230,45 @@
               </tr>
             </tbody>
           </table>
-        </div>
+
+
+
+
+       </div>
+       <div class="demo-inline-spacing">
+        <nav aria-label="Page navigation">
+          <ul class="pagination justify-content-end">
+            <li class="page-item prev">
+              <a class="page-link" href="javascript:void(0);"
+                ><i class="tf-icon bx bx-chevrons-left"></i
+              ></a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="javascript:void(0);">1</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="javascript:void(0);">2</a>
+            </li>
+            <li class="page-item active">
+              <a class="page-link" href="javascript:void(0);">3</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="javascript:void(0);">4</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" href="javascript:void(0);">5</a>
+            </li>
+            <li class="page-item next">
+              <a class="page-link" href="javascript:void(0);"
+                ><i class="tf-icon bx bx-chevrons-right"></i
+              ></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+
+
       </div>
       <!--/ Striped Rows -->
 
