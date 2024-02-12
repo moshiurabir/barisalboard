cc5aa0559d390c98b533e96485108453f2c8cdf9
moshiurabir <moshiurabir@gmail.com>
2024-02-12 15:52:57 +0600

    kk

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('session_id')->nullable();
             $table->string('is_admin')->nullable();
             $table->string('role')->nullable();
             $table->string('section')->nullable();
